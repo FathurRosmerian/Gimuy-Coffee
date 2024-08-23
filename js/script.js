38852f4c8f06ed12af9f45b43a9394a70c12afb2
Patur <fathur197808@gmail.com>
2024-08-23 13:14:56 +0700

    belajar coding

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika Hamburger Di Klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik Di Luar Sidebar Untuk Menghilangkan Nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
