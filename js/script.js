32849e31e10cf2eda5f6a17feeec361fd38cedfc
Patur <fathur197808@gmail.com>
2024-08-27 21:08:24 +0700

    Topioka Gimuy

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 // Toggle class active Hamburger Menu
 const navbarNav = document.querySelector(".navbar-nav");
 // Ketika Hamburger Di Klik
-document.querySelector('#hamburger-menu').onclick = (e) => {
-  navbarNav.classList.toggle('active');
-  e.preventDefault()
+document.querySelector("#hamburger-menu").onclick = (e) => {
+  navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
 // Toggle class active Search Menu
-const searchForm = document.querySelector('.search-form');
-const searchBox = document.querySelector('#search-box');
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
-document.querySelector('#search-button').onclick = (e) => {
-  searchForm.classList.toggle('active');
-  e.preventDefault()
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  e.preventDefault();
 };
 
 // Klik Di Luar elemen
-const hm = document.querySelector('#hamburger-menu');
-const sb = document.querySelector('#search-button');
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
