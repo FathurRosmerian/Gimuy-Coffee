78660ae8598bbbf310d285a0c399c7b56e3f8fe0
Patur <fathur197808@gmail.com>
2024-08-27 20:49:44 +0700

    part-2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
-// Toggle class active
+// Toggle class active Hamburger Menu
 const navbarNav = document.querySelector(".navbar-nav");
 // Ketika Hamburger Di Klik
-document.querySelector("#hamburger-menu").onclick = () => {
-  navbarNav.classList.toggle("active");
+document.querySelector('#hamburger-menu').onclick = (e) => {
+  navbarNav.classList.toggle('active');
+  e.preventDefault()
 };
 
-// Klik Di Luar Sidebar Untuk Menghilangkan Nav
-const hamburger = document.querySelector("#hamburger-menu");
+// Toggle class active Search Menu
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  e.preventDefault()
+};
+
+// Klik Di Luar elemen
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
